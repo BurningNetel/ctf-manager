b46e677f1965eb57cb9688aa0c6042803692dd39
BurningNetel <joepseuren1@gmail.com>
2016-01-11 15:58:36 +0100

    /events/ returns basic HTML

--- CTFmanager/tests.py
+++ CTFmanager/tests.py
@@ -1,3 +1,19 @@
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from django.test import TestCase
 
-# Create your tests here.
+from CTFmanager.views import events_page
+
+
+class EventPageTest(TestCase):
+
+    def test_events_url_resolves_to_events_page(self):
+        found = resolve('/events/')
+        self.assertEqual(found.func, events_page)
+
+    def test_events_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = events_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>CTFman - Events</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
--- functional_tests/test_event_creation.py
+++ functional_tests/test_event_creation.py
@@ -1,6 +1,6 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import unittest
+
+from selenium import webdriver
 
 
 class NewEventTests(unittest.TestCase):
@@ -13,31 +13,50 @@ class NewEventTests(unittest.TestCase):
 
     def test_can_create_an_event_from_event_page_and_retrieve_it_later(self):
         # a user goes to the events page
-        self.browser.get('http://127.0.0.1:8000')
+        self.browser.get('http://127.0.0.1:8000/events/')
+
         # He checks the pages' title is correct
         self.assertIn('CTFman - Events', self.browser.title)
+        self.assertIn(self.browser.current_url, '/events/')
+
         # the user wants to add a new event,
         # so he clicks on the button to add a new event
+        btn_add_event = self.browser.find_element_by_id('btn_add_event')
+        self.assertEqual(btn_add_event.get_attribute('text'), 'Add Event')
+        btn_add_event.click()
 
         # The browser redirects to a new page
-
-        # Check the urls path is correct
+        self.assertIn(self.browser.current_url, '/events/new')
 
         # The users fills in all the mandatory data
-
         # The events name
+        tb_name = self.browser.find_element_by_id('tb_event_name')
+        tb_name.send_keys('Hacklu')
+        self.assertEqual(tb_name.get_attribute('placeholder'), 'Name')
 
         # The date and time that the event starts
+        datetime = self.browser.find_element_by_id('datetime')
+        datetime.send_keys('20-01-2016')
+        self.assertEqual(datetime.get_attribute('placeholder'),'dd-mm-yyyy')
 
-        # Finally the user selects a category
-
-        # Then, the user clicks the 'add' button
+        # Then, the user clicks the 'confirm' button
         # When every necessary field has been filled
+        btn_confirm = self.browser.find_element_by_id('btn_confirm')
+        self.assertEqual(btn_confirm.text, 'Confirm')
 
         # The browser redirects the user to the events page
+        self.assertIn(self.browser.current_url, '/events')
+        self.assertNotIn(self.browser.current_url, '/new')
 
         # The new event is now visible on the events page
+        table = self.browser.find_element_by_id('table_events')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+                any(row.text == 'Hacklu' for row in rows)
+        )
 
+        # another CTF is coming soon, the user wants to add it to the app
+        self.fail('Finish tests!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
