989ce5a86043908e9765b41d55ec6d6c8feb4cef
BurningNetel <joepseuren1@gmail.com>
2016-01-13 11:43:08 +0100

    Refactor: Using base template to start implementing styling with bootstrap

--- CTFmanager/tests/test_views.py
+++ CTFmanager/tests/test_views.py
@@ -1,6 +1,5 @@
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
-from django.template.loader import render_to_string
 from django.test import TestCase
 from django.utils import timezone, formats
 from django.utils.timezone import timedelta
@@ -15,12 +14,6 @@ class EventPageTest(TestCase):
     def test_events_url_resolves_to_events_page(self):
         response = resolve('/events/')
         self.assertEqual(response.func, events_page)
-
-    def test_events_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = events_page(request)
-        expected_html = render_to_string('events.html')
-        self.assertEqual(response.content.decode(), expected_html)
 
     def test_events_page_renders_events_template(self):
         response = self.client.get('/events/')
@@ -78,13 +71,6 @@ class NewEventsPageTest(TestCase):
     def test_add_events_url_resolves_to_add_events_page(self):
         response = resolve('/events/new/')
         self.assertEqual(response.func, new_event_page)
-
-    def test_add_events_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = new_event_page(request)
-        expected_html = render_to_string('add_event.html', {'form': EventForm()})
-        self.assertEqual(response.status_code, 200)
-        self.assertMultiLineEqual(response.content.decode(), expected_html)
 
     def test_add_events_page_renders_add_events_template(self):
         response = self.client.get('/events/new/')
