25ae3f6988f162c531d97bec19b30a12ebd5bdd1
BurningNetel <joepseuren1@gmail.com>
2016-01-13 12:48:26 +0100

    Refactor: functional tests now start it's own server to test on.

--- functional_tests/test_event_creation.py
+++ functional_tests/test_event_creation.py
@@ -1,20 +1,24 @@
-import unittest
+from django.test import LiveServerTestCase
+from django.utils import timezone, formats
+from django.utils.timezone import timedelta
+from selenium.webdriver.firefox.webdriver import WebDriver
 
-from selenium import webdriver
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
+class NewEventTests(LiveServerTestCase):
 
-class NewEventTests(StaticLiveServerTestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+    @classmethod
+    def setUpClass(cls):
+        super(NewEventTests,cls).setUpClass()
+        cls.browser = WebDriver()
 
-    def tearDown(self):
-        self.browser.quit()
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super(NewEventTests, cls).tearDownClass()
 
     def test_can_create_an_event_from_event_page_and_retrieve_it_later(self):
         # a user goes to the events page
-        self.browser.get('http://127.0.0.1:8000/events/')
+        self.browser.get(self.live_server_url + '/events/')
 
         # He checks the pages' title is correct
         self.assertIn('CTFman - Events', self.browser.title)
@@ -37,10 +41,18 @@ class NewEventTests(StaticLiveServerTestCase):
 
         # The date and time that the event starts
         datetime = self.browser.find_element_by_id('id_date')
-        datetime.clear()
-        datetime.send_keys('2016-10-12')
         self.assertEqual('yyyy-mm-dd',datetime.get_attribute('placeholder'))
+        # The date of the upcoming event is filled in the date textbox
+        datetime.clear()
 
+        date = timezone.now() + timedelta(days=1)
+        formatted_date = formats.date_format(date, "SHORT_DATETIME_FORMAT")
+        datetime.send_keys(str(date.year) + '-'+
+                           ('0' + str(date.month))[-2:] + '-'
+                           + ('0' + str(date.day))[-2:] + " "
+                           + str(date.hour) + ":"
+                           + str(date.minute)
+                           )
         # Then, the user clicks the 'confirm' button
         # When every necessary field has been filled
         btn_confirm = self.browser.find_element_by_id('btn_submit')
@@ -53,14 +65,17 @@ class NewEventTests(StaticLiveServerTestCase):
         self.assertNotIn('/new/', self.browser.current_url)
 
         # The new event is now visible on the events page
-        # table = self.browser.find_element_by_id('table_events')
-        # rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #         any(row.text == 'Hacklu' for row in rows)
-        # )
+        table = self.browser.find_element_by_tag_name('table')
+        rows = table.find_elements_by_tag_name('td')
+        self.assertTrue(
+                any(row.text == 'Hacklu' for row in rows)
+        )
+        self.assertTrue(
+            any(row.text == formatted_date for row in rows)
+        )
 
         # another CTF is coming soon, the user wants to add it to the app
         # self.fail('Finish tests!')
 
 if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+    LiveServerTestCase.main(warnings='ignore')
