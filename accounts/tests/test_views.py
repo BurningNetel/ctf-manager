4afdbe7538ac1889a5993c537edef1bdbd6ac6cd
BurningNetel <joepseuren1@gmail.com>
2016-01-20 12:57:07 +0100

    unittest register uses correct template

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
+
 from ..views import register_page
 
 
@@ -7,3 +8,8 @@ class RegistrationTest(TestCase):
     def test_registration_url_resolves_to_registration_view(self):
         response = resolve(reverse('register'))
         self.assertEqual(response.func, register_page)
+
+    def test_registration_uses_registration_template(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'register.html')
