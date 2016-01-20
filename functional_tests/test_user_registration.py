d0ab0ced25f30570097aaca34242973329bb398c
BurningNetel <joepseuren1@gmail.com>
2016-01-20 12:22:45 +0100

    Added user registration functional test

---
+++ functional_tests/test_user_registration.py
@@ -0,0 +1,31 @@
+from .base import FunctionalTest
+from django.contrib.auth.models import User
+
+
+class RegistrationTest(FunctionalTest):
+    def test_registration_of_normal_user_and_logging_in(self):
+        # User is at registration page
+        self.browser.get('/auth/register')
+        self.assertEqual(self.browser.title, 'CTFman - Register')
+        username = self.browser.find_element_by_id('id_username')
+        password1 = self.browser.find_element_by_id('id_password1')
+        password2 = self.browser.find_element_by_id('id_password2')
+        name = 'username123'
+        password = 'v3rry_s3cur3_p4ssw0rd'
+        # User fills in username and password
+        username.send_keys(name)
+        password1.send_keys(password)
+        password2.send_keys(password)
+
+        # User confirms action
+        self.browser.find_element_by_id('btn_submit').click()
+
+        # User is redirected to login page and logs in with username and password
+        username_login = self.browser.find_element_by_id('id_username')
+        password_login = self.browser.find_element_by_id('id_password2')
+        username_login.send_keys(name)
+        password_login.send_keys(password)
+        # user submits form
+        self.browser.find_element_by_id('btn_submit').click()
+        # User is redirected to home page
+        self.assertEqual(self.browser.title, 'CTFman - Home')
