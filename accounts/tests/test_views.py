29649a3439f13178918fd8b280e8b15b64879b8d
BurningNetel <joepseuren1@gmail.com>
2016-01-20 12:42:25 +0100

    test registration url resovles to view

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,2 +1,9 @@
+from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
+from ..views import register_page
 
+
+class RegistrationTest(TestCase):
+    def test_registration_url_resolves_to_registration_view(self):
+        response = resolve(reverse('register'))
+        self.assertEqual(response.func, register_page)
