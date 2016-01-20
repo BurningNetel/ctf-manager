a0bcccf2da52447bf09da9a25fc986bcb21d56f7
BurningNetel <joepseuren1@gmail.com>
2016-01-20 12:50:11 +0100

    registration url now resolves to registration view

--- ctfmanager/urls.py
+++ ctfmanager/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     # Examples:
     url(r'^$', 'CTFmanager.views.home_page', name='home'),
     url(r'^events/', include('CTFmanager.urls')),
+    url(r'accounts/register', include('accounts.urls'))
     # url(r'^admin/', include(admin.site.urls)),
 ]
