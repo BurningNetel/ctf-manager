e156e3bcb78f0479e53f600beb3639168688d210
BurningNetel <joepseuren1@gmail.com>
2016-01-25 19:50:14 +0100

    seperated tests for forms in seperate files for each form class

--- CTFmanager/tests/forms/test_challenge.py
+++ CTFmanager/tests/forms/test_challenge.py
@@ -1,40 +1,8 @@
 from django.test import TestCase
 from django.utils import timezone
 
+from CTFmanager.forms import ChallengeForm, EMPTY_FIELD_ERROR, DUPLICATE_ERROR
 from CTFmanager.models import Event, Challenge
-from ..forms import EventForm, ChallengeForm, EMPTY_FIELD_ERROR, DUPLICATE_ERROR
-
-
-class EventFormTest(TestCase):
-    def test_form_renders_Event_inputs(self):
-        form = EventForm()
-        p = form.as_p()
-        self.assertIn('placeholder="Name"', p)
-        self.assertIn('class="form-control"', p)
-        self.assertIn('placeholder="yyyy-mm-dd (h24-MM)"', p)
-
-    def test_form_validation_for_blank_items(self):
-        form = EventForm(data={'name': '',
-                               'date': ''})
-        self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['name'], [EMPTY_FIELD_ERROR])
-        self.assertEqual(form.errors['date'], [EMPTY_FIELD_ERROR])
-
-    def test_form_validation_for_correct_items(self):
-        form = EventForm(data={'name': 'HackCTF',
-                               'date': '2015-10-01'})
-        self.assertTrue(form.is_valid())
-
-    def test_form_validation_for_incorrect_date(self):
-        form = EventForm(data={'name': 'HackCTF',
-                               'date': '01-10-2015'})
-        self.assertFalse(form.is_valid())
-
-    def test_form_validation_invalid_character_in_name(self):
-        form_whitespace = EventForm(data={'name': 'white-_ space', 'date': '01-01-2016'})
-        form_strange_chars = EventForm(data={'name': 'a1~!@#$%^&*()', 'date': '01-01-2016'})
-        self.assertFalse(form_whitespace.is_valid())
-        self.assertFalse(form_strange_chars.is_valid())
 
 
 class ChallengeFormTest(TestCase):
@@ -77,4 +45,3 @@ class ChallengeFormTest(TestCase):
         form_strange_chars = ChallengeForm(data={'name': 'a1~!@#$%^&*()', 'points': '50'})
         self.assertFalse(form_whitespace.is_valid())
         self.assertFalse(form_strange_chars.is_valid())
-
---
+++ CTFmanager/tests/forms/test_event.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+
+from CTFmanager.forms import EventForm, EMPTY_FIELD_ERROR
+
+
+class EventFormTest(TestCase):
+    def test_form_renders_Event_inputs(self):
+        form = EventForm()
+        p = form.as_p()
+        self.assertIn('placeholder="Name"', p)
+        self.assertIn('class="form-control"', p)
+        self.assertIn('placeholder="yyyy-mm-dd (h24-MM)"', p)
+
+    def test_form_validation_for_blank_items(self):
+        form = EventForm(data={'name': '',
+                               'date': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['name'], [EMPTY_FIELD_ERROR])
+        self.assertEqual(form.errors['date'], [EMPTY_FIELD_ERROR])
+
+    def test_form_validation_for_correct_items(self):
+        form = EventForm(data={'name': 'HackCTF',
+                               'date': '2015-10-01'})
+        self.assertTrue(form.is_valid())
+
+    def test_form_validation_for_incorrect_date(self):
+        form = EventForm(data={'name': 'HackCTF',
+                               'date': '01-10-2015'})
+        self.assertFalse(form.is_valid())
+
+    def test_form_validation_invalid_character_in_name(self):
+        form_whitespace = EventForm(data={'name': 'white-_ space', 'date': '01-01-2016'})
+        form_strange_chars = EventForm(data={'name': 'a1~!@#$%^&*()', 'date': '01-01-2016'})
+        self.assertFalse(form_whitespace.is_valid())
+        self.assertFalse(form_strange_chars.is_valid())
