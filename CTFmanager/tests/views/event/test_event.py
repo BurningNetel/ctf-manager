d8d128b2eca71160516a046c8524abb87d1256b6
BurningNetel <joepseuren1@gmail.com>
2016-01-25 20:58:31 +0100

    Refactored event view tests classes into seperated files

--- CTFmanager/tests/views/event/test_event.py
+++ CTFmanager/tests/views/event/test_event.py
@@ -1,12 +1,10 @@
 from django.core.urlresolvers import resolve, reverse
 from django.utils import timezone
-from django.utils.html import escape
 from django.utils.timezone import timedelta
 
-from CTFmanager.forms import EventForm, EMPTY_FIELD_ERROR
 from CTFmanager.models import Event, Challenge
-from CTFmanager.views import events_page, new_event_page, view_event
-from .base import ViewTestCase
+from CTFmanager.tests.views.base import ViewTestCase
+from CTFmanager.views import events_page, view_event
 
 
 class EventPageTest(ViewTestCase):
@@ -62,53 +60,6 @@ class EventPageTest(ViewTestCase):
         archive = response.context['archive']
         self.assertEqual(len(archive), 0)
         self.assertContains(response, 'No past events!')
-
-
-class NewEventsPageTest(ViewTestCase):
-    def post_incorrect_form(self):
-        return self.client.post(
-                '/events/new/',
-                data={'name': '', 'date': '2016-10-02'})
-
-    def test_login_required(self):
-        self.client.logout()
-        response = self.client.get(reverse('newEvent'))
-        self.assertRedirects(response, reverse('login') + '?next=' + reverse('newEvent'))
-
-    def test_add_events_url_resolves_to_add_events_page(self):
-        response = resolve(reverse('newEvent'))
-        self.assertEqual(response.func, new_event_page)
-
-    def test_add_events_page_renders_add_events_template(self):
-        response = self.client.get(reverse('newEvent'))
-        self.assertTemplateUsed(response, 'event/add_event.html')
-
-    def test_add_events_page_renders_event_form(self):
-        response = self.client.get(reverse('newEvent'))
-        self.assertIsInstance(response.context['form'], EventForm)
-
-    def test_for_invalid_input_renders_error_text(self):
-        response = self.post_incorrect_form()
-        self.assertContains(response, escape(EMPTY_FIELD_ERROR))
-
-    def test_for_invalid_input_nothing_saved(self):
-        self.post_incorrect_form()
-        self.assertEqual(Event.objects.count(), 0)
-
-    def test_for_invalid_input_renders_add_events_page(self):
-        response = self.post_incorrect_form()
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'event/add_event.html')
-
-    def test_for_invalid_input_passes_event_form_to_template(self):
-        response = self.post_incorrect_form()
-        self.assertIsInstance(response.context['form'], EventForm)
-
-    def test_for_valid_input_renders_event_template(self):
-        response = self.client.post(
-                reverse('newEvent'),
-                data={'name': 'hatstack', 'date': '2016-10-02'})
-        self.assertRedirects(response, reverse('events'))
 
 
 class EventPageDetailTest(ViewTestCase):
---
+++ CTFmanager/tests/views/event/test_new_event.py
@@ -0,0 +1,54 @@
+from django.core.urlresolvers import reverse, resolve
+from django.utils.html import escape
+
+from CTFmanager.forms import EventForm, EMPTY_FIELD_ERROR
+from CTFmanager.models import Event
+from CTFmanager.tests.views.base import ViewTestCase
+from CTFmanager.views import new_event_page
+
+
+class NewEventsPageTest(ViewTestCase):
+    def post_incorrect_form(self):
+        return self.client.post(
+                '/events/new/',
+                data={'name': '', 'date': '2016-10-02'})
+
+    def test_login_required(self):
+        self.client.logout()
+        response = self.client.get(reverse('newEvent'))
+        self.assertRedirects(response, reverse('login') + '?next=' + reverse('newEvent'))
+
+    def test_add_events_url_resolves_to_add_events_page(self):
+        response = resolve(reverse('newEvent'))
+        self.assertEqual(response.func, new_event_page)
+
+    def test_add_events_page_renders_add_events_template(self):
+        response = self.client.get(reverse('newEvent'))
+        self.assertTemplateUsed(response, 'event/add_event.html')
+
+    def test_add_events_page_renders_event_form(self):
+        response = self.client.get(reverse('newEvent'))
+        self.assertIsInstance(response.context['form'], EventForm)
+
+    def test_for_invalid_input_renders_error_text(self):
+        response = self.post_incorrect_form()
+        self.assertContains(response, escape(EMPTY_FIELD_ERROR))
+
+    def test_for_invalid_input_nothing_saved(self):
+        self.post_incorrect_form()
+        self.assertEqual(Event.objects.count(), 0)
+
+    def test_for_invalid_input_renders_add_events_page(self):
+        response = self.post_incorrect_form()
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'event/add_event.html')
+
+    def test_for_invalid_input_passes_event_form_to_template(self):
+        response = self.post_incorrect_form()
+        self.assertIsInstance(response.context['form'], EventForm)
+
+    def test_for_valid_input_renders_event_template(self):
+        response = self.client.post(
+                reverse('newEvent'),
+                data={'name': 'hatstack', 'date': '2016-10-02'})
+        self.assertRedirects(response, reverse('events'))
