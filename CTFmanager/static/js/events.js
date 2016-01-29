21233703efba8bba26e15574b8c2ed555b57c51d
BurningNetel <joepseuren1@gmail.com>
2016-01-29 15:09:53 +0100

    Refactored events.js

--- CTFmanager/static/js/events.js
+++ CTFmanager/static/js/events.js
@@ -1,59 +1,23 @@
 $( document ).ready(function() {
+    btn_post_handler();
 
-    $(".btn-post").click(function (e) {
-        /** Updates the 'participating' count. **/
-        e.preventDefault();
-        e.stopPropagation();
-
-        var event_name = $(this).closest('a').attr('id');
-        var url = "/events/" + event_name + "/users/";
-        var p_join_count = $('#' + event_name + '-join-count');
-        var username = $('#username').text();
-
-        if($(this).text() == "Join") {
-            $.ajax({
-                type: "POST",
-                url: url,
-                success: function (data) {
-                    var members = data['members'];
-                    if (data['status_code'] == 200) {
-                        $('#' + event_name + '-join-count').text(members + " Participating!");
-                        p_join_count.attr('data-content', p_join_count.attr('data-content') + '\n' + username)
-                        $(e.target).removeClass('btn-primary').addClass('btn-warning').text('Leave');
-                    }
-                }
-            });
-        }
-        else
-        {
-            $.ajax({
-                type: "DELETE",
-                url: url,
-                success: function (data) {
-                    var members = data['members'];
-                    if (data['status_code'] == 200) {
-                        p_join_count.text(members + " Participating!");
-                        if(members > 0)
-                        {
-                            p_join_count.attr('data-content', p_join_count.attr('data-content').replace(username, ''));
-                        }
-                        else {
-                            p_join_count.attr('data-content', 'Nobody has joined yet!')
-                        }
-                        $(e.target).removeClass('btn-warning').addClass('btn-primary').text('Join');
-                    }
-                }
-            });
-        }
-    });
-
+    /** prevent page from going to next page if clicked on an element inside 'a' tag. **/
     $(".sp").click(function (e){
         e.preventDefault();
         e.stopPropagation();
     });
 
+    /** Initialize Bootstrop popover **/
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    });
 
-    // CSRF code
+    csrf_setup();
+
+});
+
+function csrf_setup() {
+    /** Django required setup for csrf validation  **/
     function getCookie(name) {
         var cookieValue = null;
         var i = 0;
@@ -84,9 +48,53 @@ $( document ).ready(function() {
             }
         }
     });
+}
 
-});
+function btn_post_handler() {
+    /** Handles click event on Join/Leave event button in upcoming events list. **/
+    $(".btn-post").click(function (e) {
+        /** Updates the 'participating' count. **/
+        e.preventDefault();
+        e.stopPropagation();
 
-$(function () {
-    $('[data-toggle="popover"]').popover()
-});
+        var event_name = $(this).closest('a').attr('id');
+        var url = "/events/" + event_name + "/users/";
+        var p_join_count = $('#' + event_name + '-join-count');
+        var username = $('#username').text();
+
+        if ($(this).text() == "Join") {
+            $.ajax({
+                type: "POST",
+                url: url,
+                success: function (data) {
+                    var members = data['members'];
+                    if (data['status_code'] == 200) {
+                        $('#' + event_name + '-join-count').text(members + " Participating!");
+                        p_join_count.attr('data-content', p_join_count.attr('data-content') + '\n' + username)
+                        $(e.target).removeClass('btn-primary').addClass('btn-warning').text('Leave');
+                    }
+                }
+            });
+        }
+        else {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    var members = data['members'];
+                    if (data['status_code'] == 200) {
+                        p_join_count.text(members + " Participating!");
+                        if (members > 0) {
+                            p_join_count.attr('data-content', p_join_count.attr('data-content').replace(username, ''));
+                        }
+                        else {
+                            p_join_count.attr('data-content', 'Nobody has joined yet!')
+                        }
+                        $(e.target).removeClass('btn-warning').addClass('btn-primary').text('Join');
+                    }
+                }
+            });
+        }
+    });
+}
+
