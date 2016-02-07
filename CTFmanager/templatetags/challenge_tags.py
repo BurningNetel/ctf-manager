cecbf904def2dd380fc8a0a9ddf46fcd64d1262b
BurningNetel <joepseuren1@gmail.com>
2016-02-07 21:22:38 +0100

    fix in challenge_tags templatetag

--- CTFmanager/templatetags/challenge_tags.py
+++ CTFmanager/templatetags/challenge_tags.py
@@ -10,6 +10,8 @@ register = template.Library()
 def solve_time(user, challenge):
     try:
         solve_dt = Solver.objects.get(user=user, challenge=challenge).solve_time
-        return "You solved this challenge at %s" % date_format(solve_dt, 'SHORT_DATETIME_FORMAT', True)
+        if solve_dt:
+            return "You solved this challenge at %s" % date_format(solve_dt, 'SHORT_DATETIME_FORMAT', True)
+        return ''
     except Solver.DoesNotExist:
         return ''
